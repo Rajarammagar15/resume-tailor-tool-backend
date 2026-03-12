aca0a7a289b116f498664f7276ee9cae8955d21f
rajarammagar15 <rajarammagar15@gmail.com>
2026-03-12 09:54:00 +0530

    exception handler added

--- src/main/java/com/rajaram/resumetailor/controller/GlobalExceptionHandler.java
+++ src/main/java/com/rajaram/resumetailor/controller/GlobalExceptionHandler.java
@@ -1,24 +1,28 @@
-//package com.rajaram.resumetailor.controller;
-//
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.ErrorResponse;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.RestControllerAdvice;
-//
-//@RestControllerAdvice
-//public class GlobalExceptionHandler {
-//
-//    @ExceptionHandler(PdfParsingException.class)
-//    public ResponseEntity<ErrorResponse> handlePdfError(PdfParsingException e) {
-//        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-//            .body(new ErrorResponse("PDF_PARSE_ERROR", e.getMessage()));
-//    }
-//
-//    @ExceptionHandler(OpenAiException.class)
-//    public ResponseEntity<ErrorResponse> handleAiError(OpenAiException e) {
-//        // Don't expose OpenAI errors to users
-//        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
-//            .body(new ErrorResponse("AI_SERVICE_ERROR", "AI service temporarily unavailable"));
-//    }
-//}
+package com.rajaram.resumetailor.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handle(Exception ex) {
+        log.error("Unhandled exception", ex);
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Unexpected error occurred");
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleBadRequest(Exception ex) {
+        log.error("Unhandled exception", ex);
+        return ResponseEntity
+                .badRequest()
+                .body(ex.getMessage());
+    }
+}
